a5aebd855a28c592a6d1984c40b9c1cba358c5fa
Valdez <lavn4877@gmail.com>
2022-02-10 23:39:35 -0600

    Mi primer Hola mundo en Python

---
+++ 1.py
@@ -0,0 +1,22 @@
+cantidadPersonas = 100
+
+cantidadPersonas+=50
+print(cantidadPersonas)
+
+#este es un comentario y asi se puede hacer xd
+''''este
+
+es un comentario
+
+de muchas
+lineas perros xd
+
+
+
+'''
+
+
+nombre,edad = 'luisxd' ,30
+
+
+print(nombre,edad)
